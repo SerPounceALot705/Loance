180d353d9341d667731a428f1e0ae8f49f91cea5
Valerie Dmitryuk <cosmic.pony705@gmail.com>
2021-05-05 02:28:44 +0800

    Добавлен underline header-menu item, анимация кнопок

--- components/Header.vue
+++ components/Header.vue
@@ -1,47 +1,62 @@
 <template>
   <section class="header">
     <a href="/" class="header-logo-container">
-      <img class="header-logo" alt="logo" :src="require('~/assets/images/logo.png')" />
+      <img
+        class="header-logo"
+        alt="logo"
+        :src="require('~/assets/images/logo.png')"
+      />
       <img
         class="header-name"
         alt="logo-name"
         :src="require('~/assets/images/loance.png')"
       />
     </a>
-    <ul class="header-menu">
-      <li>
-        <a href="" class="header-link">
-          <img
-            class="header-menu-img_accounting"
-            alt="accounting"
-            :src="require('~/assets/images/001-accounting.png')"
-          />
-          <span class="header-menu-text">Займ</span>
-        </a>
-      </li>
-      <li>
-        <a href="" class="header-link">
-          <img
-            class="header-menu-img"
-            alt="investments"
-            :src="require('~/assets/images/001-return.png')"
-          />
-          <span class="header-menu-text">Инвестиции</span>
-        </a>
-      </li>
-    </ul>
-    <ul class="header-transaction">
-      <li>
-        <a href="" class="header-link header-link-transaction">
-          <img
-            class="header-menu-img"
-            alt="transaction"
-            :src="require('~/assets/images/002-transaction.png')"
-          />
-          <a href="/TransactionHistory" class="header-menu-text">Транзакции</a>
-        </a>
-      </li>
-    </ul>
+    <div class="header-menu-item-container">
+      <ul class="header-menu">
+        <li>
+          <a href="" class="header-link">
+            <img
+              class="header-menu-img_accounting"
+              alt="accounting"
+              :src="require('~/assets/images/001-accounting.png')"
+            />
+            <span class="header-menu-text">Займ</span>
+          </a>
+          <div class="header-menu-underline header-menu-underline_actived"></div>
+        </li>
+        <li>
+          <a href="" class="header-link">
+            <img
+              class="header-menu-img"
+              alt="investments"
+              :src="require('~/assets/images/001-return.png')"
+            />
+            <span class="header-menu-text">Инвестиции</span>
+          </a>
+        </li>
+      </ul>
+      <div class="header-menu-underline"></div>
+    </div>
+    <div class="header-menu-item-container">
+      <ul class="header-transaction">
+        <li>
+          <a href="" class="header-link header-link-transaction">
+            <img
+              class="header-menu-img"
+              alt="transaction"
+              :src="require('~/assets/images/002-transaction.png')"
+            />
+            <a href="/TransactionHistory" class="header-menu-text"
+              >Транзакции</a
+            >
+          </a>
+        </li>
+      </ul>
+      <div
+        class="header-menu-underline header-menu-underline_transaction"
+      ></div>
+    </div>
 
     <div class="header-notifications-container">
       <a href="">
@@ -64,7 +79,7 @@
 
 
 <style lang="scss">
-  @import '~assets/styles/Header/styles.scss';
+@import "~assets/styles/Header/styles.scss";
 </style>
 
 <script>
